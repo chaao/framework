aa82bbcef22687f08177143927774b73fdc27ee2
Evan <chaaoli@gmail.com>
2017-04-12 15:10:44 +0800

    update
    Signed-off-by: evan <chaaoli@gmail.com>

--- baseFramework/src/main/java/baseFramework/exception/BaseException.java
+++ baseFramework/src/main/java/baseFramework/exception/BaseException.java
@@ -1,5 +1,10 @@
 package baseFramework.exception;
 
+import com.google.common.collect.Lists;
+import org.apache.logging.log4j.message.Message;
+
+import java.util.List;
+
 /**
  * @author chao.li
  * @date 2016年11月30日
@@ -7,45 +12,67 @@ package baseFramework.exception;
 public class BaseException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-	private String formatter;
-	private Object[] params;
+	private StringBuilder formatter;
+	private List<Object> params;
+
+	public BaseException() {
+
+		super();
+		this.formatter = new StringBuilder(getClass().getSimpleName()).append("|");
+	}
 
 	public BaseException(String message) {
 		super(message);
-		this.formatter = message;
+		this.formatter = new StringBuilder(getClass().getSimpleName()).append("|").append(message);
 	}
 
 	/**
 	 * no message (replaced by exception name)
-	 * 
-	 * @param formatter
-	 *            log-msg-formatter
-	 * @param params
-	 *            log-msg-params
+	 *
+	 * @param formatter log-msg-formatter
+	 * @param params    log-msg-params
 	 */
 	public BaseException(String formatter, Object... params) {
 		super();
-		this.formatter = formatter;
-		this.params = params;
+		this.formatter = new StringBuilder(getClass().getSimpleName()).append("|").append(formatter);
+		this.params = Lists.newArrayList(params);
 	}
 
 	public BaseException(Throwable cause, String message) {
 		super(message, cause);
-		this.formatter = message;
+		this.formatter = new StringBuilder(getClass().getSimpleName()).append("|").append(message);
 	}
 
 	/**
 	 * no message (replaced by exception name)
-	 * 
-	 * @param formatter
-	 *            log-msg-formatter
-	 * @param params
-	 *            log-msg-params
+	 *
+	 * @param formatter log-msg-formatter
+	 * @param params    log-msg-params
 	 */
 	public BaseException(Throwable cause, String formatter, Object... params) {
 		super(cause);
-		this.formatter = formatter;
-		this.params = params;
+		this.formatter = new StringBuilder(getClass().getSimpleName()).append("|").append(formatter);
+		this.params = Lists.newArrayList(params);
+	}
+
+	public void addMessage(String formatter, Object... params) {
+		this.formatter.append(" ").append(formatter);
+
+		if (this.params != null) {
+			for (Object item : params) {
+				this.params.add(item);
+			}
+		} else {
+			this.params = Lists.newArrayList(params);
+		}
+	}
+
+	public Message getMsg() {
+		if (params != null) {
+			return MsgFactory.build(this.formatter.toString(), params);
+		} else {
+			return MsgFactory.build(this.formatter.toString());
+		}
 	}
 
 	@Override

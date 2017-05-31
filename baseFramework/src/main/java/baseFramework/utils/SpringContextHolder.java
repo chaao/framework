82dd97184bc9992eff156438900ccac336d6af3e
Evan <chao.li@inveno.cn>
2017-05-31 15:38:32 +0800

    update

--- baseFramework/src/main/java/baseFramework/utils/SpringContextHolder.java
+++ baseFramework/src/main/java/baseFramework/utils/SpringContextHolder.java
@@ -1,66 +1,79 @@
 package baseFramework.utils;
 
-import java.util.Map;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.EmbeddedValueResolverAware;
+import org.springframework.util.StringValueResolver;
+
+import java.util.Map;
 
 /**
  * 以静态变量保存Spring ApplicationContext, 可在任何代码任何地方任何时候中取出ApplicaitonContext.
- * 
+ *
  * @author chao.li
  * @date 2017年1月9日
  */
-public class SpringContextHolder implements ApplicationContextAware {
-	private static ApplicationContext applicationContext;
+public class SpringContextHolder implements ApplicationContextAware, EmbeddedValueResolverAware {
+    private static ApplicationContext applicationContext;
+    private static StringValueResolver stringValueResolver;
 
-	/**
-	 * 实现ApplicationContextAware接口的context注入函数, 将其存入静态变量.
-	 */
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) {
-		SpringContextHolder.applicationContext = applicationContext;
-	}
+    /**
+     * 实现ApplicationContextAware接口的context注入函数, 将其存入静态变量.
+     */
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        SpringContextHolder.applicationContext = applicationContext;
+    }
 
-	/**
-	 * 取得存储在静态变量中的ApplicationContext.
-	 */
-	public static ApplicationContext getApplicationContext() {
-		checkApplicationContext();
-		return applicationContext;
-	}
+    @Override
+    public void setEmbeddedValueResolver(StringValueResolver stringValueResolver) {
+        SpringContextHolder.stringValueResolver = stringValueResolver;
+    }
 
-	@SuppressWarnings("unchecked")
-	public static <T> T getBean(String name) {
-		checkApplicationContext();
-		return (T) applicationContext.getBean(name);
-	}
+    /**
+     * 取得存储在静态变量中的ApplicationContext.
+     */
+    public static ApplicationContext getApplicationContext() {
+        checkApplicationContext();
+        return applicationContext;
+    }
 
-	public static <T> T getBean(String name, Class<T> clazz) {
-		checkApplicationContext();
-		return applicationContext.getBean(name, clazz);
-	}
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name) {
+        checkApplicationContext();
+        return (T) applicationContext.getBean(name);
+    }
 
-	public static <T> T getBean(Class<T> clazz) {
-		checkApplicationContext();
-		return applicationContext.getBean(clazz);
-	}
+    public static <T> T getBean(String name, Class<T> clazz) {
+        checkApplicationContext();
+        return applicationContext.getBean(name, clazz);
+    }
 
-	public static <T> Map<String, T> getBeansOfType(Class<T> clazz) {
-		checkApplicationContext();
-		return applicationContext.getBeansOfType(clazz);
-	}
+    public static <T> T getBean(Class<T> clazz) {
+        checkApplicationContext();
+        return applicationContext.getBean(clazz);
+    }
 
-	/**
-	 * 清除applicationContext静态变量.
-	 */
-	public static void cleanApplicationContext() {
-		applicationContext = null;
-	}
+    public static <T> Map<String, T> getBeansOfType(Class<T> clazz) {
+        checkApplicationContext();
+        return applicationContext.getBeansOfType(clazz);
+    }
 
-	private static void checkApplicationContext() {
-		if (applicationContext == null) {
-			throw new IllegalStateException("applicaitonContext未注入,请在applicationContext.xml中定义SpringContextHolder");
-		}
-	}
+    /**
+     * 清除applicationContext静态变量.
+     */
+    public static void cleanApplicationContext() {
+        applicationContext = null;
+    }
+
+    private static void checkApplicationContext() {
+        if (applicationContext == null) {
+            throw new IllegalStateException("applicaitonContext未注入,请在applicationContext.xml中定义SpringContextHolder");
+        }
+    }
+
+    public static String getPropertiesValue(String name) {
+        name = "${" + name + "}";
+        return stringValueResolver.resolveStringValue(name);
+    }
 }

bf7ad4cb69eaf976fc3d9f4f914611685c65415e
Evan <chao.li@inveno.cn>
2017-11-03 14:38:02 +0800

    update

---
+++ baseFramework/src/main/java/baseFramework/utils/Base64Tools.java
@@ -0,0 +1,30 @@
+package baseFramework.utils;
+
+import com.google.common.base.Charsets;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Base64;
+
+/**
+ * @author chao.li
+ * @date 2017/11/3
+ */
+public class Base64Tools {
+
+	public static String encode(String src) {
+		if (StringUtils.isNotBlank(src))
+			return Base64.getEncoder().encodeToString(src.getBytes(Charsets.UTF_8));
+		else
+			return null;
+	}
+
+	public static String decode(String src) {
+		if (StringUtils.isNotBlank(src)) {
+			byte[] decode = Base64.getDecoder().decode(src);
+			return new String(decode, Charsets.UTF_8);
+		} else {
+			return null;
+		}
+	}
+
+}
